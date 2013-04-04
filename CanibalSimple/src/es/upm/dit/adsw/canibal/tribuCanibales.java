8343588132f2ce4c85144d2d3b13697c650170af
Juan A. de la Puente <jpuente@dit.upm.es>
2013-04-04 17:31:04 +0200

    Pasado a utf-8

--- CanibalSimple/src/es/upm/dit/adsw/canibal/tribuCanibales.java
+++ CanibalSimple/src/es/upm/dit/adsw/canibal/tribuCanibales.java
@@ -4,15 +4,15 @@ public class tribuCanibales {
 	public static void main(String[] args) {
 
 		int nRacionesMax = 4;
-		int tamañoTribu  = 5;
+		int tama√±oTribu  = 5;
 		
 		marmita laMarmita = new marmita(nRacionesMax);
 		
 		cocinero elCocinero = new cocinero (laMarmita);
 		elCocinero.start();
 		
-		canibal[] canibales  = new canibal[tamañoTribu];		
-		for (int i = 0; i < tamañoTribu; i++){
+		canibal[] canibales  = new canibal[tama√±oTribu];		
+		for (int i = 0; i < tama√±oTribu; i++){
 			try{
 				Thread.sleep(1000);
 				canibales[i] = new canibal(laMarmita, i);
