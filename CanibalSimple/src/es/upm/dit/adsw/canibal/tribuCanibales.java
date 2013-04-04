d027b37956cd61e29feacb42cfe96324059c3968
Alejandro Alonso <aalonso@dit.upm.es>
2013-04-04 17:34:34 +0200

    Eliminados ñ

--- CanibalSimple/src/es/upm/dit/adsw/canibal/tribuCanibales.java
+++ CanibalSimple/src/es/upm/dit/adsw/canibal/tribuCanibales.java
@@ -4,15 +4,15 @@ public class tribuCanibales {
 	public static void main(String[] args) {
 
 		int nRacionesMax = 4;
-		int tamañoTribu  = 5;
+		int tamanoTribu  = 5;
 		
 		marmita laMarmita = new marmita(nRacionesMax);
 		
 		cocinero elCocinero = new cocinero (laMarmita);
 		elCocinero.start();
 		
-		canibal[] canibales  = new canibal[tamañoTribu];		
-		for (int i = 0; i < tamañoTribu; i++){
+		canibal[] canibales  = new canibal[tamanoTribu];		
+		for (int i = 0; i < tamanoTribu; i++){
 			try{
 				Thread.sleep(1000);
 				canibales[i] = new canibal(laMarmita, i);
