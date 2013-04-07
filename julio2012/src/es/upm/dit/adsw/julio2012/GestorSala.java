4c38b132493e369c5b2128be0d53eedcbff076c3
Alejandro Alonso <aalonsom@gmail.com>
2013-04-07 19:44:09 +0200

    Gestor de Sala

---
+++ julio2012/src/es/upm/dit/adsw/julio2012/GestorSala.java
@@ -0,0 +1,43 @@
+package es.upm.dit.adsw.julio2012;
+
+public class GestorSala {
+
+	private int nPersonas = 0;
+	private int nMaxPersonasNormalT = 50;
+	private int nMaxPersonasAltaT   = 35;
+	private int nMaxPersonas = nMaxPersonasNormalT;
+	private int tUmbral = 30;
+	private int nJubilados = 0;
+
+	public synchronized void entrarSalaJubilado()
+			throws InterruptedException {
+		nJubilados ++;
+		while (nPersonas >= nMaxPersonas) {
+			wait();
+		}
+		nJubilados --;
+		nPersonas ++;
+	}
+
+	public synchronized void entrarSala ()
+			throws InterruptedException {
+		while (nPersonas >= nMaxPersonas || nJubilados > 0)
+		{
+			// Espero si no pueden entrar m'as personas
+			wait();
+		}
+		nPersonas++;
+	}
+
+	public synchronized void salirSala ()
+			throws InterruptedException {
+		nPersonas--;
+		notifyAll();
+	}
+
+	public synchronized void notificarTemperatura (int temperatura)
+	{
+		if (temperatura > tUmbral) nMaxPersonas = nMaxPersonasAltaT;
+		if (temperatura < tUmbral) nMaxPersonas = nMaxPersonasNormalT;
+	}
+}
